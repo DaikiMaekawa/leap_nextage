385fd364e738d5a164fd0f954be84bae3d9de0c4
DaikiMaekawa <method_aspect_card@yahoo.co.jp>
2014-06-14 17:22:16 +0900

    移動に使用するパラメータを変更

--- scripts/leap_nextage_node.py
+++ scripts/leap_nextage_node.py
@@ -15,30 +15,34 @@ import geometry_msgs.msg
 import copy
 import tf
 import math
+import threading
 
 from moveit_commander.exception import MoveItCommanderException
 from leap_motion2.msg import Hand
 
-class LeapNextage(object):
-    MAX_ARM_POS_X = 0.4
-    MIN_ARM_POS_X = 0.1
-    MAX_ARM_POS_Y = -0.1
-    MIN_ARM_POS_Y = -0.3
-    MAX_ARM_POS_Z = 0.4
-    MIN_ARM_POS_Z = -0.1
-    
-    MAX_LEAP_POS_X = 1.0
-    MIN_LEAP_POS_X = -1.0
-    MAX_LEAP_POS_Y = 2.0
-    MIN_LEAP_POS_Y = 0.3
-    MAX_LEAP_POS_Z = 1.0
-    MIN_LEAP_POS_Z = -1.0
-    
-    def __init__(self):
-        rospy.init_node("moveit_command_sender")
-        rospy.Subscriber("leapmotion2/data", Hand, self.__callback_hands_data)
-        self.__robot = moveit_commander.RobotCommander()
+import Leap
 
+class LeapReceiver(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        rospy.Subscriber("leapmotion2/data", Hand, self.__callback_hands_data)
+        self.__hand_data = None
+
+    def __callback_hands_data(self, msg):
+        rospy.loginfo(rospy.get_name() + ": Leap ROS Data %s" % msg)
+        self.__hand_data = msg
+
+    @property
+    def hand_data(self):
+        return self.__hand_data
+
+class LeapNextage(object):
+    R_HAND_HOME_X = 0
+    R_HAND_HOME_Y = 1.0
+    R_HAND_HOME_Z = 0
+            
+    def __init__(self):
+        self.__robot = moveit_commander.RobotCommander()
         self.__rarm = moveit_commander.MoveGroupCommander("right_arm")
         self.__larm = moveit_commander.MoveGroupCommander("left_arm")
         self.__waypoints = []
@@ -47,12 +51,14 @@ class LeapNextage(object):
         
         target_pose_r = target_pose_l = geometry_msgs.msg.Pose()
         target_pose_r.position.x = 0.2035
-        target_pose_r.position.y = -0.25
+        target_pose_r.position.y = -0.45
         target_pose_r.position.z = 0.0709
-        target_pose_r.orientation.x = 0.000427
-        target_pose_r.orientation.y = 0.000317
-        target_pose_r.orientation.z = -0.000384
-        target_pose_r.orientation.w = 0.999999
+        
+        target_pose_r.orientation.x = -0.003
+        target_pose_r.orientation.y = -0.708
+        target_pose_r.orientation.z = -0.003
+        target_pose_r.orientation.w = 0.706
+                
         self.__rarm.set_pose_target(target_pose_r)
         self.__rarm.go()
 
@@ -60,92 +66,74 @@ class LeapNextage(object):
         self.__larm.set_pose_target(target_pose_l)
         self.__larm.go()
 
-        self.__old_hand_data = None
-        self.__hand_data = None
-    
+        self.__leap_receiver = LeapReceiver()
+        self.__leap_receiver.setDaemon(True)
+        self.__leap_receiver.start()
+
+    """
     def __callback_hands_data(self, msg):
         rospy.loginfo(rospy.get_name() + ": Leap ROS Data %s" % msg)
         self.__hand_data = msg
-
-        """
-        if not self.__old_hand_data:
-            self.__old_hand_data = msg
-            return
-
-        wpose = geometry_msgs.msg.Pose()
-        pos = self.__rarm.get_current_pose().pose.position
-
-        wpose.position.x = pos.x + (msg.palmpos.z - self.__old_hand_data.palmpos.z) * 0.01
-        wpose.position.y = pos.y + (msg.palmpos.x - self.__old_hand_data.palmpos.x) * 0.01
-        wpose.position.z = pos.z + (msg.palmpos.y - self.__old_hand_data.palmpos.y) * 0.01
-        wpose.orientation.x = -0.003
-        wpose.orientation.y = -0.708
-        wpose.orientation.z = -0.003
-        wpose.orientation.w = 0.706
-        
-        wpose.position.x = wpose.position.x if self.MIN_ARM_POS_X < wpose.position.x < self.MAX_ARM_POS_X else pos.x
-        wpose.position.y = wpose.position.y if self.MIN_ARM_POS_Y < wpose.position.y < self.MAX_ARM_POS_Y else pos.y
-        wpose.position.z = wpose.position.z if self.MIN_ARM_POS_Z < wpose.position.z < self.MAX_ARM_POS_Z else pos.z
-        
-        self.__rarm.clear_pose_targets()
-        self.__rarm.set_pose_target(wpose)
-        self.__rarm.go()
-        """
-
-        """
-        self.__waypoints.append(copy.deepcopy(wpose))
-        
-        if len(self.__waypoints) > 5: self.__rarm.clear_pose_targets()
-            (plan, fraction) = self.__rarm.compute_cartesian_path(self.__waypoints, 0.05, 0.0)
-            self.__rarm.execute(plan)
-            print "=" * 10, " plan..."
-            self.__waypoints = []
-
-        self.__old_hands_data = msg
-        """
+    """
 
     def run(self):
-        rate = rospy.Rate(20)
-        old_hand_data = None
-        waypoints = []
-
         while not rospy.is_shutdown():
-            rate.sleep()
-            if self.__hand_data:
-                if not old_hand_data:
-                    old_hand_data = self.__hand_data
-                    continue
-                    
-                hand_data = self.__hand_data
-                wpose = geometry_msgs.msg.Pose()
-                pos = self.__rarm.get_current_pose().pose.position
+            if self.__leap_receiver.hand_data:
+                hand_data = self.__leap_receiver.hand_data
+                pose = self.__rarm.get_current_pose().pose
 
-                wpose.position.x = pos.x + (hand_data.palmpos.z - old_hand_data.palmpos.z) * 0.01
-                wpose.position.y = pos.y + (hand_data.palmpos.x - old_hand_data.palmpos.x) * 0.01
-                wpose.position.z = pos.z + (hand_data.palmpos.y - old_hand_data.palmpos.y) * 0.01
-                wpose.orientation.x = -0.003
-                wpose.orientation.y = -0.708
-                wpose.orientation.z = -0.003
-                wpose.orientation.w = 0.706
-                
-                #wpose.position.x = wpose.position.x if self.MIN_ARM_POS_X < wpose.position.x < self.MAX_ARM_POS_X else pos.x
-                #wpose.position.y = wpose.position.y if self.MIN_ARM_POS_Y < wpose.position.y < self.MAX_ARM_POS_Y else pos.y
-                #wpose.position.z = wpose.position.z if self.MIN_ARM_POS_Z < wpose.position.z < self.MAX_ARM_POS_Z else pos.z
-                
-                if len(waypoints) < 10:
-                    waypoints.append(wpose)
-                else:
-                    self.__rarm.clear_pose_targets()
-                    (plan, fraction) = self.__rarm.compute_cartesian_path(waypoints, 0.05, 0.0)
-                    self.__rarm.execute(plan)
-                    del waypoints[:]
-                    
-                old_hand_data = hand_data
+                print "diff.x = ", hand_data.ypr.y * 0.001
+                print "diff.y = ", hand_data.ypr.x * 0.001
+
+                pose.position.x += -hand_data.ypr.x * 0.001
+                pose.position.y += hand_data.ypr.y * 0.001
+
+                print "ypr: "
+                print hand_data.ypr
+                self.__rarm.set_pose_target(pose)
+                self.__rarm.go(wait = False)
+                rospy.sleep(2)
+
         
-        #rospy.spin()
+        """
+        waypoints = []
+        future_pose = None
+        pose_count = 0
+        while not rospy.is_shutdown():
+            if self.__leap_receiver.hand_data:
+                hand_data = self.__leap_receiver.hand_data
+                
+                if not future_pose:
+                    pose = self.__rarm.get_current_pose().pose
+                else:
+                    pose = future_pose
+                
+                #pose.position.y += 0.01
+
+                q = tf.transformations.quaternion_from_euler(hand_data.ypr.z, hand_data.ypr.y, hand_data.ypr.x)
+                #q = tf.transformations.quaternion_from_euler(3.14, -1.57, -3.14)
+                pose.orientation.x = q[0]
+                pose.orientation.y = q[1]
+                pose.orientation.z = q[2]
+                pose.orientation.w = q[3]
+
+                pose_count += 1
+                
+                if pose_count >= 1:
+                    self.__rarm.set_pose_target(pose)
+                    self.__rarm.go(wait = False)
+                    rospy.sleep(1)
+                    pose_count = 0
+                    future_pose = None
+                else:
+                    future_pose = pose
+
+            rospy.sleep(0.3)
+        """
 
 if __name__ == '__main__':
     try:
+        rospy.init_node("moveit_command_sender")
         leap_nextage = LeapNextage()
         leap_nextage.run()
     except MoveItCommanderException:
